ce3c141a331b2d4f42321ead3aee6fec47855863
hiimstevejin <jingle5695@gmail.com>
2025-06-02 17:14:58 -0500

    remove unused SVG files and add Hero component to the homepage

---
+++ kiso-app/src/components/hero.tsx
@@ -0,0 +1,26 @@
+export default function Hero() {
+  return (
+    <>
+      <div>
+        <h1>Lorem ipsum dolor sit amet consectetur adipiscing elit.</h1>
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque
+            faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi
+            pretium tellus duis convallis. Tempus leo eu aenean sed diam urna
+            tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas.
+          </p>
+        </div>
+        <div>
+          <button>Learn More...</button>
+        </div>
+      </div>
+      <div>
+        <img
+          src="https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60"
+          alt="place holder"
+        />
+      </div>
+    </>
+  );
+}
