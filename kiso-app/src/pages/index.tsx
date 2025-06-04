f91f4f46dbc89598d64219efe5d8865a21ff32e0
hiimstevejin <jingle5695@gmail.com>
2025-06-04 11:14:27 -0500

    add Banner component to homepage and integrate news ticker

--- kiso-app/src/pages/index.tsx
+++ kiso-app/src/pages/index.tsx
@@ -1,7 +1,27 @@
-import Hero from '../components/hero';
+import Banner from "@/components/banner";
+import Hero from "../components/hero";
+import { useState } from "react";
+import ToggleButton from "@/components/toggleButton";
+
+const loremArray = [
+  "June 4: Kiso App Launches with Exciting Features!",
+  "June 5: New Update Available - Check Out the Latest Improvements!",
+  "June 6: Kiso App Reaches 1,000 Downloads - Thank You for Your Support!",
+  "June 7: Kiso App Now Supports Dark Mode - Switch It On!",
+  "June 8: Kiso App Introduces New User Interface - Try It Out!",
+  "June 9: Kiso App Partners with Local Businesses Discover Exclusive Offers!",
+];
 
 export default function Home() {
+  const [toggle, setToggle] = useState(false);
   return (
-    <Hero/>
+    <>
+      <Banner newsArray={loremArray} />
+      <ToggleButton
+        toggled={toggle}
+        onToggle={() => setToggle((prev) => !prev)}
+      />
+      <Hero />
+    </>
   );
 }
